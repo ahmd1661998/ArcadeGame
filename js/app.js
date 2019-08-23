39fc565b9937298db736b757daded05cc9d9a2bc
Ahmed Osama <ahmedOsamaBedawy@users.noreply.github.com>
2019-08-23 09:44:07 +0200

    add steps create function

--- js/app.js
+++ js/app.js
@@ -1,39 +1,75 @@
-// Enemies our player must avoid
+// Enemies our player must avoid ========Delete
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+    // Variables applied to each of our instances go here, ========Delete
+    // we've provided one for you to get started ========Delete
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+
+    // x pos
+    // y pos
+
+
+    // The image/sprite for our enemies, this uses ========Delete
+    // a helper we've provided to easily load images ========Delete
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Update the enemy's position, required method for game ========Delete
+// Parameter: dt, a time delta between ticks ========Delete
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // You should multiply any movement by the dt parameter ========Delete
+    // which will ensure the game runs at the same speed for ========Delete
+    // all computers. ========Delete
+
+    //if enemy is not passed boundary
+        // move forward
+        // Increment x by peed * dt
+    // else
+        // Reset pos to start
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen, required method for game ========Delete
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+}; 
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Now write your own player class ========Delete
+// This class requires an update(), render() and ========Delete
+// a handleInput() method. ========Delete
+
+// Hero class
+
+    //==> constructor
+
+        //==> properties
+            //==> x pos
+            //==> y pos
+            //==> sprite images
+
+        //==> Methods
+            //==> Update position
+                //==> Check collision here
+                    //==> Did player x and y collide with enemy?
+                //==> Check win here?
+                    //==> Did player x and y reach final tile?
+
+            // Rander 
+                // Draw player spirte on current x and y coord position
+
+            // Handle keyboard input
+                //Update player's x and y property aording to input
+            
+            // Reset Hero
+                // Set x nd y to starting x and y
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Now instantiate your objects. ========Delete
+// Place all enemy objects in an array called allEnemies ========Delete
+// Place the player object in a variable called player ========Delete
 
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to your ========Delete
+// Player.handleInput() method. You don't need to modify this. ========Delete
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
